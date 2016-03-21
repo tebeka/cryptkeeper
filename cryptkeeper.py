74c1828099856370b43c989c9e87936b76855663
Miki Tebeka <miki.tebeka@gmail.com>
2016-03-21 15:16:34 +0200

    using config

--- cryptkeeper.py
+++ cryptkeeper.py
@@ -3,9 +3,28 @@
 from subprocess import check_call, check_output
 from os import makedirs, environ
 from os.path import dirname, expanduser, isdir
+import sys
+
+if sys.version_info[:2] < (3, 0):
+    from ConfigParser import ConfigParser
+else:
+    from configparser import ConfigParser
 
 
 rc_dir = expanduser('~/.config/cryptkeeper')
+
+
+def load_config():
+    cp = ConfigParser()
+    cp.read(['%s/crypts.ini' % rc_dir])
+    crypts = {}  # name -> path
+    for name in cp.sections():
+        path = cp.get(name, 'path')
+        if path.startswith('~'):
+            path = expanduser(path)
+        crypts[name] = path
+
+    return crypts
 
 
 def free_dev():
@@ -14,10 +33,6 @@ def free_dev():
 
 def tc_name(name):
     return '%s/tcs/%s.tc' % (rc_dir, name)
-
-
-def vol_name(name):
-    return '%s/vols/%s' % (rc_dir, name)
 
 
 def mapper_name(name):
@@ -30,7 +45,7 @@ def ensure_dir(fname):
         makedirs(root)
 
 
-def create(name, size):
+def create(name, path, size):
     dev = free_dev()
     tc = tc_name(dev)
     mapper = mapper_name(name)
@@ -52,6 +67,8 @@ def create(name, size):
     for cmd in cmds:
         check_call(cmd)
 
+    update_config(name, path)
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -65,6 +82,7 @@ if __name__ == '__main__':
     subps = parser.add_subparsers(help='sub-command help')
     crp = subps.add_parser('create', help='create crypt')
     crp.add_argument('name', help='crypt name')
+    crp.add_argument('path', help='crypt volume path')
     crp.add_argument('--size', help='crypt size (e.g. 2G)', default='1G')
     crp.set_defaults(func=runargs(create))
     args = parser.parse_args()
